59315a17741b32edee9fc3c35db487a385291860
KirstenC2 <runners620@gmail.com>
2025-06-11 17:42:38 +0800

    feat:explanation pages

--- routes/kafka.js
+++ routes/kafka.js
@@ -9,5 +9,13 @@ router.get('/consumer', kafkaController.index);
 // function wise
 router.post('/send', kafkaController.sendMessage);
 
+// explanation page
+router.get('/kafka/explanation', (req, res) => {
+  res.render('kafka-explanation');
+});
+router.get('/explanation-zh', (req, res) => {
+  res.render('kafka-explanation-zh');
+});
+
 
 module.exports = router;
